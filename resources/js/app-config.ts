ad3a9f6254d83a650889bc4c6a9b6d46590f5e9e
Mojahidul Islam <itsemon245@gmail.com>
2024-12-03 20:24:38 +0600

    problem in presiting layout

--- resources/js/app-config.ts
+++ resources/js/app-config.ts
@@ -1,13 +1,23 @@
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import BaseLayout from "./Layouts/BaseLayout";
+import { ReactNode } from "react";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 const appConfig = {
     title: (title: string) => `${title} - ${appName}`,
-    resolve: (name: string) =>
-        resolvePageComponent(
-            `./Pages/${name}.tsx`,
-            import.meta.glob('./Pages/**/*.tsx'),
-        ),
+    resolve: (name: string) => {
+        const pages = import.meta.glob('./Pages/**/*.jsx')
+        let currentPage = `./Pages/${name}.tsx`
+        let page = resolvePageComponent(currentPage, pages) as any;
+        console.log(page)
+        if (page) {
+            page.default.layout = page.default.layout || BaseLayout({ children: page })
+        }
+        console.log('after', page)
+        return page;
+    }
+
 }
 export default appConfig;
