8c8fec9503002158e8aca214fd808d6f540df64b
Mojahidul Islam <itsemon245@gmail.com>
2025-03-01 13:23:32 +0600

    fix: buy sell text

--- resources/js/Pages/Marketplace/Index.tsx
+++ resources/js/Pages/Marketplace/Index.tsx
@@ -12,6 +12,9 @@ export default function Index() {
     const { user } = useAuth()
     const [ads, setAds] = useState(initialAds)
     const wallets = usePage().props.wallets as AddMethodData[]
+    const getType = (ad?: AddData) => {
+        return user.id === ad?.owner_id ? ad?.type : (ad?.type == 'Buy' ? 'Sell' : 'Buy')
+    }
     const Header = ({ ad }: { ad: AddData }) => {
         return (
             <div className="flex py-2 md:py-3 px-3 items-center border-b">
@@ -28,7 +31,7 @@ export default function Index() {
     const Footer = ({ ad }: { ad: AddData }) => {
         return (
             <div className="flex items-center justify-end pb-2">
-                <Button className="uppercase" label={ad.type} severity={ad.type == 'Buy' ? 'success' : 'danger'} size="small" onClick={() => setSelectedAd(ad)} />
+                <Button className="uppercase" label={getType(ad)} severity={getType(ad) === 'Buy' ? 'success' : 'danger'} size="small" onClick={() => setSelectedAd(ad)} />
             </div>
         )
     }
@@ -57,7 +60,7 @@ export default function Index() {
             <Head title="Marketplace" />
             <SquareBg />
             <ClassicNav />
-            <Dialog header={selectedAd?.type + ' ' + selectedAd?.addMethod?.name}
+            <Dialog header={getType(selectedAd) + ' ' + selectedAd?.addMethod?.name}
                 pt={{
                     content: {
                         className: "!py-0"
@@ -69,7 +72,7 @@ export default function Index() {
                 className="max-w-max"
                 visible={selectedAd !== undefined} onHide={() => setSelectedAd(undefined)}>
                 <div className="md:px-5 min-w-[300px]">
-                    <h2 className="heading text-center tracking-wide">Do you want to <span className={cn(selectedAd?.type == "Buy" ? 'text-green-500' : 'text-red-500')}>{selectedAd?.type}</span>?</h2>
+                    <h2 className="heading text-center tracking-wide">Do you want to <span className={cn(getType(selectedAd) == "Buy" ? 'text-green-500' : 'text-red-500')}>{getType(selectedAd)}</span>?</h2>
                     <div className="text-base font-semibold">User ID: #{selectedAd?.owner_id}</div>
                     <div className="text-base font-semibold">Contact: {selectedAd?.contact}</div>
 
@@ -98,12 +101,11 @@ export default function Index() {
                     <form onSubmit={submit} className="flex items-center gap-2 my-3">
                         <label className={cn("cursor-pointer font-bold py-2 px-3 rounded-md transition-colors", type == 'buy' ? 'bg-primary text-white' : 'bg-white text-primary')}>
                             <input type="radio" name="type" value={type as string} checked={type == 'buy'} className="sr-only peer" onChange={() => setType('buy')} />
-                            Buy
+                            Sell
                         </label>
                         <label className={cn("cursor-pointer font-bold py-2 px-3 rounded-md transition-colors", type == 'sell' ? 'bg-primary text-white' : 'bg-white text-primary')}>
                             <input type="radio" name="type" value={type as string} checked={type == 'sell'} className="sr-only peer" onChange={() => setType('sell')} />
-
-                            Sell
+                            Buy
                         </label>
                         <select onChange={(e) => {
                             setWalletId(e.target.value)
