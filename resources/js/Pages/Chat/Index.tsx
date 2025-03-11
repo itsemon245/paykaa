e3883b3d654799e3b2fb773cfc2d5281b0dfbe7d
Mojahidul Islam <itsemon245@gmail.com>
2025-03-12 00:15:52 +0600

    fix: admin chats on mobile

--- resources/js/Pages/Chat/Index.tsx
+++ resources/js/Pages/Chat/Index.tsx
@@ -3,13 +3,14 @@ import useBreakpoint from "@/Hooks/useBrakpoints";
 export default function Index() {
     const { chats, setChats, fetchChats } = useChat();
     const [isMobile, setIsMobile] = useState(false);
+    const { isAdmin } = useAuth();
     const { max } = useBreakpoint();
     useEffect(() => {
         setIsMobile(max('md'));
     }, [window.innerWidth]);
     return (
         <>
-            {(route().current('chat.index') || !isMobile) && <ChatSidebar chats={chats} setChats={setChats} fetchChats={fetchChats} />}
+            {(route().current('chat.index') || !isMobile || (route().current('helpline') && isAdmin)) && <ChatSidebar chats={chats} setChats={setChats} fetchChats={fetchChats} />}
             <div className={`main h-full ${route().current('chat.show') ? '!right-0' : ''}`}>
                 <div className="flex flex-col h-full w-full justify-center items-center">
                     <div className="text-3xl font-bold text-gray-500">
