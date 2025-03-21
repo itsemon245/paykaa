4930cc3b13aacda99719fbdea387326059d90856
Mojahidul Islam <itsemon245@gmail.com>
2025-03-21 21:44:38 +0600

    clickable links

--- resources/js/Components/Chat/TextMessage.tsx
+++ resources/js/Components/Chat/TextMessage.tsx
@@ -1,11 +1,21 @@
 import { cn } from "@/lib/utils"
 import { MessageData } from "@/types/_generated"
+import linkifyHtml from 'linkify-html';
 
 export default function TextMessage({
     message,
 }: { message: MessageData }) {
+    const body = linkifyHtml(message.body ?? '', {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+        className: cn('!font-bold !underline', message.by_me ? '!text-gray-800 hover:!text-gray-300' : '!text-primary-500 hover:!text-primary-600'),
+        attributes: {
+            target: '_blank',
+            rel: 'noopener noreferrer',
+        }
+    })
     return (
-        <div className={cn("whitespace-pre-line")}>{message.body}</div>
+        <div className="whitespace-pre-line" dangerouslySetInnerHTML={{ __html: body }} />
     )
 }
 
